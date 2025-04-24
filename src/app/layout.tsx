60d10def61f34ca569d22a4fd4f409248449c4cd
Alif Essa Nurcahyani <essanurcahyania@gmail.com>
2025-04-24 23:17:41 +0800

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@
 import Navbar from "../components/Navbar";
 import NavbarSeller from "../components/NavbarSeller";
 import NavbarBuyer from "../components/NavbarBuyer";
-import NavbarDashboardSeller from "../components/NavbarDashboardSeller";
 import Sidebar from "@/components/Sidebar";
 import Footer from "../components/Footer";
 import { usePathname } from "next/navigation";
@@ -20,12 +19,6 @@ export default function RootLayout({
     pathname === "/auth/login" || pathname === "/auth/register";
 
   const isSellerPage = pathname.startsWith("/seller");
-
-  const isSellerDashboardPage =
-    pathname.startsWith("/seller/my-product") ||
-    pathname.startsWith("/seller/transaction") ||
-    pathname.startsWith("/seller/chat");
-
   const isBuyerPage = pathname.startsWith("/buyer");
   const isAdminPage = pathname.startsWith("/admin");
 
