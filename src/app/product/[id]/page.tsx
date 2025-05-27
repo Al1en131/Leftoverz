8d99ba85951dcda09b0704881709a1b6327bf33e
Alif Essa Nurcahyani <essanurcahyania@gmail.com>
2025-05-27 19:53:48 +0700

    update

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -162,7 +162,7 @@ useEffect(() => {
                 alt={product.name}
                 width={600}
                 height={600}
-                className="w-full lg:h-96 max-lg:h-72 object-cover rounded-lg"
+                className="w-full lg:h-96 max-lg:h-72 object-cover border border-blue-400 rounded-lg"
               />
             )}
           </div>
@@ -174,7 +174,7 @@ useEffect(() => {
                   alt={`Thumbnail ${index}`}
                   width={100}
                   height={100}
-                  className="lg:w-24 lg:h-24 max-lg:h-20 max-lg:w-full object-cover rounded-lg"
+                  className="lg:w-24 lg:h-24 max-lg:h-20 max-lg:w-full border border-blue-400 object-cover rounded-lg"
                 />
               </button>
             ))}
