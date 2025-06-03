2bbb981befb77533222a1b52332e50522e65feba
Alif Essa Nurcahyani <essanurcahyania@gmail.com>
2025-06-03 21:51:50 +0700

    update

--- src/app/buyer/product/buy-product/[id]/page.tsx
+++ src/app/buyer/product/buy-product/[id]/page.tsx
@@ -283,7 +283,7 @@ export default function BuyProduct() {
   return (
     <>
       <Script
-        src="https://app.sandbox.midtrans.com/snap/snap.js"
+        src="https://app.midtrans.com/snap/snap.js"
         data-client-key={process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY}
         strategy="afterInteractive"
       />
