4ae9959829ff9ad10e615a5f34cc7e0d273d5d55
Alif Essa Nurcahyani <essanurcahyania@gmail.com>
2025-06-03 20:23:17 +0700

    update dark and light mode in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,26 @@
+import { useTheme } from "next-themes";
+import { useEffect } from "react";
+
 export default function Footer() {
+  const { theme, setTheme } = useTheme();
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme && storedTheme !== theme) {
+      setTheme(storedTheme);
+    }
+  }, [theme, setTheme]);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "dark" ? "light" : "dark";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
   return (
-    <footer className="dark:bg-[#080B2A] bg-blue-400 text-white text-center">
+    <footer
+      className={`text-white text-center ${
+        theme === "dark" ? "bg-[#080B2A]" : "bg-blue-400"
+      }`}
+    >
       <div className="bg-white/5 p-4">
         <p>&copy; 2025 Leftoverz. All rights reserved.</p>
       </div>
