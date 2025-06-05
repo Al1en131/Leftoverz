df2a0feb97aa7bfc720e5727b40cd2d3edf66957
Alif Essa Nurcahyani <essanurcahyania@gmail.com>
2025-06-05 12:26:23 +0700

    update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -16,3 +16,7 @@ export function middleware(request: NextRequest) {
 
   return NextResponse.next()
 }
+
+export const config = {
+  matcher: ['/seller/:path*', '/admin/:path*', '/buyer/:path*'],
+}
