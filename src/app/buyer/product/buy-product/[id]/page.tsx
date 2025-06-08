21a3b024c73b891fefc742dd7160b4b36ebc63cf
Alif Essa Nurcahyani <essanurcahyania@gmail.com>
2025-06-08 23:32:55 +0700

    update

--- src/app/buyer/product/buy-product/[id]/page.tsx
+++ src/app/buyer/product/buy-product/[id]/page.tsx
@@ -354,6 +354,37 @@ export default function BuyProduct() {
             </div>
           </div>
         )}
+        {showIncompleteProfilePopup && (
+          <div className="fixed inset-0 bg-black/55 flex items-center justify-center z-50">
+            <div className="bg-[#080B2A] border-yellow-400 border z-50 rounded-lg py-8 px-14 shadow-lg text-center">
+              <div className="flex justify-center mb-4">
+                <Image
+                  src="/images/warning.svg" // Ganti jika belum punya
+                  width={80}
+                  height={80}
+                  alt="Warning"
+                  className="w-20 h-20"
+                />
+              </div>
+              <h2 className="text-2xl font-bold mb-1 text-yellow-400">
+                Lengkapi Profil
+              </h2>
+              <p className="mb-6 text-yellow-400">
+                Harap lengkapi alamat dan data wilayah Anda di halaman Edit
+                Profile terlebih dahulu.
+              </p>
+              <button
+                onClick={() => {
+                  setShowIncompleteProfilePopup(false);
+                  router.push("/buyer/detail-profile");
+                }}
+                className="bg-yellow-400 hover:bg-yellow-500 text-white font-semibold py-2 px-6 rounded-full"
+              >
+                Edit Profil
+              </button>
+            </div>
+          </div>
+        )}
         <Image
           width={100}
           height={100}
