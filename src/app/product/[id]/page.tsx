989fb69f39e5ab70726c22d6ed08d3b43a123cc8
Alif Essa Nurcahyani <essanurcahyania@gmail.com>
2025-06-10 12:39:13 +0700

    update

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -174,7 +174,7 @@ export default function ProductDetail() {
               />
             </svg>
           </div>
-          <p className="text-white">Back to Products</p>
+          <p className="text-blue-400">Back to Products</p>
         </Link>
         {loading ? (
           <div className="text-center text-blue-400">Loading...</div>
