3769ea8bbea3fa6e88d8941e7bcaf62519b1b237
Alif Essa Nurcahyani <essanurcahyania@gmail.com>
2025-06-11 11:59:12 +0700

    update

--- src/app/buyer/my-order/[id]/page.tsx
+++ src/app/buyer/my-order/[id]/page.tsx
@@ -1160,11 +1160,20 @@ export default function BuyProduct() {
                               </button>
                               <button
                                 onClick={handleMarkAsDelivered}
-                                className="mt-4 mb-2 block mx-auto px-4 py-2 text-blue-400 border border-blue-400 rounded"
-                                disabled={isSubmitting}
+                                className={`mt-4 mb-2 block mx-auto px-4 py-2 rounded border ${
+                                  refund?.status_package === "delivered"
+                                    ? "bg-blue-400 text-white border-blue-400 cursor-default"
+                                    : "text-blue-400 border-blue-400 hover:bg-blue-500 hover:text-white"
+                                }`}
+                                disabled={
+                                  isSubmitting ||
+                                  refund?.status_package === "delivered"
+                                }
                               >
                                 {isSubmitting
                                   ? "Menyimpan..."
+                                  : refund?.status_package === "delivered"
+                                  ? "Pengembalian Barang Berhasil"
                                   : "Pesanan Selesai"}
                               </button>
                             </div>
@@ -1173,9 +1182,22 @@ export default function BuyProduct() {
 
                         <button
                           onClick={() => setShowStatusModal(false)}
-                          className="mt-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+                          className="absolute top-3 right-3 text-gray-500 hover:text-red-500"
                         >
-                          Tutup
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-6 w-6"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth={2}
+                              d="M6 18L18 6M6 6l12 12"
+                            />
+                          </svg>
                         </button>
                       </div>
                     </div>
