f968b30ae330a38e97951a566a5d0af49ca48374
Alif Essa Nurcahyani <essanurcahyania@gmail.com>
2025-06-13 03:22:10 +0700

    update

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -341,109 +341,110 @@ export default function User() {
             </Link>
           </div>
         </div>
-
-        <div className="w-full max-w-full overflow-x-auto">
-          <table className="w-max table-auto rounded-lg my-4">
-            <thead className="text-white text-md">
-              <tr className="border-b-2 border-[#56577A]">
-                <th className="px-4 py-3 text-center">No.</th>
-                <th className="px-4 py-3 text-left">Nama</th>
-                <th className="px-4 py-3 text-left">Email</th>
-                <th className="px-4 py-3 text-center">No. Hp</th>
-                <th className="px-4 py-3 text-center">Tipe Pembayaran</th>
-                <th className="px-4 py-3 text-center">No. Rek</th>
-                <th className="px-4 py-3 text-center">Kategori</th>
-                <th className="px-4 py-3 text-center">Aksi</th>
-              </tr>
-            </thead>
-            <tbody>
-              {loading ? (
-                <tr>
-                  <td colSpan={7} className="text-center py-10">
-                    Loading users...
-                  </td>
+        <div className="container mx-auto">
+          <div className="overflow-x-auto">
+            <table className="w-max table-auto rounded-lg my-4">
+              <thead className="text-white text-md">
+                <tr className="border-b-2 border-[#56577A]">
+                  <th className="px-4 py-3 text-center">No.</th>
+                  <th className="px-4 py-3 text-left">Nama</th>
+                  <th className="px-4 py-3 text-left">Email</th>
+                  <th className="px-4 py-3 text-center">No. Hp</th>
+                  <th className="px-4 py-3 text-center">Tipe Pembayaran</th>
+                  <th className="px-4 py-3 text-center">No. Rek</th>
+                  <th className="px-4 py-3 text-center">Kategori</th>
+                  <th className="px-4 py-3 text-center">Aksi</th>
                 </tr>
-              ) : (
-                currentUsers.map((item, index) => (
-                  <tr key={item.id} className="border-b border-[#56577A]">
-                    <td className="px-4 py-4 text-center">
-                      {index + offset + 1}
-                    </td>
-                    <td className="px-4 py-4 text-left">
-                      {item.name.length > 25
-                        ? item.name.slice(0, 25) + "..."
-                        : item.name}
-                    </td>
-                    <td className="px-4 py-4 text-left">{item.email}</td>
-                    <td className="px-4 py-4 text-center">
-                      {item.phone_number}
-                    </td>
-                    <td className="px-4 py-4 capitalize text-center">
-                      {item.payment_type || "-"}
-                    </td>
-                    <td className="px-4 py-4 text-center">
-                      {item.payment_account_number || "-"}
-                    </td>
-                    <td className="px-4 py-4 text-center">
-                      <span
-                        className={`px-4 py-2 text-sm font-semibold capitalize rounded-full ${
-                          item.role === "penjual"
-                            ? "bg-blue-700 text-white"
-                            : item.role === "pembeli"
-                            ? "bg-blue-500 text-white"
-                            : "bg-blue-400 text-white"
-                        }`}
-                      >
-                        {item.role}
-                      </span>
-                    </td>
-                    <td className="px-4 py-4 flex justify-center space-x-2">
-                      <Link
-                        href={`/admin/users/edit/${item.id}`}
-                        className="inline-flex items-center justify-center px-1 py-1 text-sm font-bold text-white bg-blue-500 rounded-md shadow hover:bg-blue-600 transition"
-                      >
-                        <svg
-                          className="w-4 h-4"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          strokeWidth={2}
-                          aria-hidden="true"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M18 5V4a1 1 0 0 0-1-1H8.914a1 1 0 0 0-.707.293L4.293 7.207A1 1 0 0 0 4 7.914V20a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-5M9 3v4a1 1 0 0 1-1 1H4m11.383.772 2.745 2.746m1.215-3.906a2.089 2.089 0 0 1 0 2.953l-6.65 6.646L9 17.95l.739-3.692 6.646-6.646a2.087 2.087 0 0 1 2.958 0Z"
-                          />
-                        </svg>
-                      </Link>
-                      <button
-                        onClick={() => confirmDelete(item.id)}
-                        className="inline-flex items-center justify-center p-1 text-sm font-bold text-white bg-red-500 rounded-md shadow hover:bg-red-600 transition"
-                      >
-                        <svg
-                          className="w-4 h-4"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          strokeWidth={1.5}
-                          aria-hidden="true"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                          />
-                        </svg>
-                      </button>
+              </thead>
+              <tbody>
+                {loading ? (
+                  <tr>
+                    <td colSpan={7} className="text-center py-10">
+                      Loading users...
                     </td>
                   </tr>
-                ))
-              )}
-            </tbody>
-          </table>
+                ) : (
+                  currentUsers.map((item, index) => (
+                    <tr key={item.id} className="border-b border-[#56577A]">
+                      <td className="px-4 py-4 text-center">
+                        {index + offset + 1}
+                      </td>
+                      <td className="px-4 py-4 text-left">
+                        {item.name.length > 25
+                          ? item.name.slice(0, 25) + "..."
+                          : item.name}
+                      </td>
+                      <td className="px-4 py-4 text-left">{item.email}</td>
+                      <td className="px-4 py-4 text-center">
+                        {item.phone_number}
+                      </td>
+                      <td className="px-4 py-4 capitalize text-center">
+                        {item.payment_type || "-"}
+                      </td>
+                      <td className="px-4 py-4 text-center">
+                        {item.payment_account_number || "-"}
+                      </td>
+                      <td className="px-4 py-4 text-center">
+                        <span
+                          className={`px-4 py-2 text-sm font-semibold capitalize rounded-full ${
+                            item.role === "penjual"
+                              ? "bg-blue-700 text-white"
+                              : item.role === "pembeli"
+                              ? "bg-blue-500 text-white"
+                              : "bg-blue-400 text-white"
+                          }`}
+                        >
+                          {item.role}
+                        </span>
+                      </td>
+                      <td className="px-4 py-4 flex justify-center space-x-2">
+                        <Link
+                          href={`/admin/users/edit/${item.id}`}
+                          className="inline-flex items-center justify-center px-1 py-1 text-sm font-bold text-white bg-blue-500 rounded-md shadow hover:bg-blue-600 transition"
+                        >
+                          <svg
+                            className="w-4 h-4"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            strokeWidth={2}
+                            aria-hidden="true"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M18 5V4a1 1 0 0 0-1-1H8.914a1 1 0 0 0-.707.293L4.293 7.207A1 1 0 0 0 4 7.914V20a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-5M9 3v4a1 1 0 0 1-1 1H4m11.383.772 2.745 2.746m1.215-3.906a2.089 2.089 0 0 1 0 2.953l-6.65 6.646L9 17.95l.739-3.692 6.646-6.646a2.087 2.087 0 0 1 2.958 0Z"
+                            />
+                          </svg>
+                        </Link>
+                        <button
+                          onClick={() => confirmDelete(item.id)}
+                          className="inline-flex items-center justify-center p-1 text-sm font-bold text-white bg-red-500 rounded-md shadow hover:bg-red-600 transition"
+                        >
+                          <svg
+                            className="w-4 h-4"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            strokeWidth={1.5}
+                            aria-hidden="true"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                            />
+                          </svg>
+                        </button>
+                      </td>
+                    </tr>
+                  ))
+                )}
+              </tbody>
+            </table>
+          </div>
         </div>
 
         <div className="flex justify-center my-6">
