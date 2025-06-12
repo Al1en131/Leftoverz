af14cccde540b8881e6352a2e1be31a26d37f7cb
Alif Essa Nurcahyani <essanurcahyania@gmail.com>
2025-06-13 03:03:23 +0700

    update

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -343,7 +343,7 @@ export default function User() {
         </div>
 
         <div className="w-full overflow-x-auto">
-          <table className="min-w-full rounded-lg my-4">
+          <table className="min-w-[1000px] rounded-lg my-4">
             <thead className="text-white text-md">
               <tr className="border-b-2 border-[#56577A]">
                 <th className="px-4 py-3 text-center">No.</th>
