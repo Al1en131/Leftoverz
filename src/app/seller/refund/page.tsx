7e7229f81cba46d17e2ff9605ffc92151e7cebc1
Alif Essa Nurcahyani <essanurcahyania@gmail.com>
2025-06-14 08:15:21 +0700

    update

--- src/app/seller/refund/page.tsx
+++ src/app/seller/refund/page.tsx
@@ -424,44 +424,40 @@ export default function Refund() {
                     <td className="px-6 py-3 text-center">
                       <div className="flex justify-center items-center gap-2">
                         {[
+                          "approved",
                           "shipping",
                           "refunded",
                           "requested",
-                          "approved",
                         ].includes(item.status) ? (
-                          item.status === "shipping" ? (
-                            <button
-                              onClick={() => {
-                                setSelectedRefund(item);
-                                handleTrackPackageRefund();
-                              }}
-                              className="px-2 py-2 bg-blue-400 text-white rounded hover:bg-blue-500"
+                          <button
+                            onClick={() => {
+                              setSelectedRefund(item);
+                              handleTrackPackageRefund();
+                            }}
+                            className="px-2 py-2 bg-blue-400 text-white rounded hover:bg-blue-500"
+                          >
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth="1.5"
+                              stroke="currentColor"
+                              className="w-4 h-4"
                             >
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke="currentColor"
-                                className="w-4 h-4"
-                              >
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
-                                />
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                                />
-                              </svg>
-                            </button>
-                          ) : (
-                            "-" // atau ganti dengan apapun jika status bukan "approved" tapi masih termasuk yang lain
-                          )
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                              />
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                              />
+                            </svg>
+                          </button>
                         ) : (
-                          "-" // status tidak masuk dalam daftar
+                          "-"
                         )}
 
                         <button
