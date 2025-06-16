22b4f4390cf044a56b31d00a393021dc5062a89f
Alif Essa Nurcahyani <essanurcahyania@gmail.com>
2025-06-16 21:44:17 +0700

    update

--- src/app/admin/refunds/page.tsx
+++ src/app/admin/refunds/page.tsx
@@ -431,36 +431,42 @@ export default function Products() {
 
                   <td className="px-3 py-4 text-white text-center align-middle">
                     <div className="flex items-center justify-center gap-2">
-                      {item.status === "shipping" ||
-                        (item.status === "refunded" && (
-                          <button
-                            onClick={() => {
-                              setSelectedRefund(item);
-                              handleTrackPackageRefund();
-                            }}
-                            className="px-1.5 py-1.5 bg-blue-400 text-white rounded hover:bg-blue-500"
+                      {[
+                        "approved",
+                        "shipping",
+                        "refunded",
+                        "requested",
+                      ].includes(item.status?.toLowerCase().trim()) ? (
+                        <button
+                          onClick={() => {
+                            setSelectedRefund(item);
+                            handleTrackPackageRefund();
+                          }}
+                          className="px-2 py-2 bg-blue-400 text-white rounded hover:bg-blue-500"
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="w-4 h-4"
                           >
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              strokeWidth="1.5"
-                              stroke="currentColor"
-                              className="w-4 h-4"
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
-                              />
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                              />
-                            </svg>
-                          </button>
-                        ))}
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                            />
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                            />
+                          </svg>
+                        </button>
+                      ) : (
+                        "-"
+                      )}
                       {showTrackingModal && trackingData && (
                         <div className="fixed inset-0 z-50 bg-black/50 flex items-center justify-center text-left">
                           <div
